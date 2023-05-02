09cf1888c0723cc30cf42c531a7951a4ca8cbbcd
John Karas <tuj47543@temple.edu>
2023-05-02 15:33:24 -0400

    Fixed a couple bugs with the bargraph, also added a second bargraph that allows the user to click on a select box to change the district and show that respective district's crimes so the user can compare two different districts.

--- bargraphs.js
+++ bargraphs.js
@@ -46,18 +46,6 @@
             typeMap.set(type.id, type.type);
         });
 
-            // add the options to the button
-    d3.select("#selectButton2")
-    .selectAll('myOptions')
-    .data(crimePerDistrict)
-    .enter()
-    .append('option')
-    .text(function (d) { return d.district_num; }) // text showed in the menu
-    .attr("value", function (d, i) { return i; }) // corresponding value returned by the button
-
-        //console.log(districtObject);
-
-
         crime.forEach((crime) => {  // this is basically a for loop
             const i = districtObject[crime.dc_dist].findIndex(e => e.type === typeMap.get(crime.ucr_general));
             if (i > -1) {
@@ -69,12 +57,6 @@
                 districtObject[crime.dc_dist].push(tempObj);
             }
 
-
-            // if (!districtObject[crime.dc_dist].has(crime.ucr_general)) {
-            //     districtObject[crime.dc_dist].set(crime.ucr_general, 1);
-            // } else {
-            //     districtObject[crime.dc_dist].set(crime.ucr_general, districtObject[crime.dc_dist].get(crime.ucr_general) + 1);
-            // }
         })
         
         crimePerDistrict.forEach(obj => {
@@ -85,104 +67,121 @@
 
         console.log(districtObject);
 
-        // set the dimensions and margins of the graph
-        var margin = { top: 10, right: 30, bottom: 30, left: 480 },
-            width = 1300 - margin.left - margin.right,
-            height = 750 - margin.top - margin.bottom;
+        function createBargraph(selectButton, graph, color, h, w) {
 
-        var svg = d3.select("#bargraph")
+            // add the options to the button
+            d3.select(selectButton)
+                .selectAll('myOptions')
+                .data(crimePerDistrict)
+                .enter()
+                .append('option')
+                .text(function (d) { return d.district_num; }) // text showed in the menu
+                .attr("value", function (d, i) { return i; }) // corresponding value returned by the button
+
+            // set the dimensions and margins of the graph
+            var margin = { top: 10, right: 30, bottom: 30, left: 285 },
+                width = w - margin.left - margin.right,
+                height = h - margin.top - margin.bottom;
+
+            var svg = d3.select(graph)
                 .attr("width", width + margin.left + margin.right)
                 .attr("height", height + margin.top + margin.bottom)
-            .append("g")
+                .append("g")
                 .attr("transform",
-                "translate(" + margin.left + "," + margin.top + ")");
+                    "translate(" + margin.left + "," + margin.top + ")");
 
-        // Add y axis
-        var y = d3.scaleBand()
-            // .domain(crimePerDistrict[0].crimes.map(function(d) {return d.type}))
-            .domain(types)
-            .range([0, height])
-            .padding(.1)
-        var yAxis = svg.append("g")
-            .call(d3.axisLeft(y));
+            // Add y axis
+            var y = d3.scaleBand()
+                .domain(types)
+                .range([0, height])
+                .padding(.1)
+            var yAxis = svg.append("g")
+                .call(d3.axisLeft(y));
 
 
-        // Add x axis
-        var x = d3.scaleLinear()
-            .domain([0, d3.max(crimePerDistrict[0].crimes, (d) => { return (d.count) })])
-            .range([0, width]);
-        var xAxis = svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x).tickSize(3));
+            // Add x axis
+            var x = d3.scaleLinear()
+                .domain([0, d3.max(crimePerDistrict[0].crimes, (d) => { return (d.count) })])
+                .range([0, width]);
+            var xAxis = svg.append("g")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x).tickSize(3))
+                .attr("font-size", "5px");
 
-        function update(data) {
+            function update(data) {
 
-            //update the y scale:
-            y.domain(data.map(function(d) {return d.type;}))
-            yAxis.transition().duration(1000).call(d3.axisLeft(y))
+                //update the y scale:
+                //data.map(function (d) { return d.type; })
+                y.domain(types)
+                yAxis.transition().duration(1000).call(d3.axisLeft(y))
 
-            //update the x scale:
-            x.domain([0, d3.max(data, (d) => {return (d.count)})])
-            xAxis.transition().duration(1000).call(d3.axisBottom(x).tickSize(3))
+                //update the x scale:
+                x.domain([0, d3.max(data, (d) => { return (d.count) })])
+                xAxis.transition().duration(1000).call(d3.axisBottom(x).tickSize(3))
 
-            var u = svg.selectAll("rect")
-                .data(data)
+                var u = svg.selectAll("rect")
+                    .data(data)
                 u
-                .enter()
-                .append("rect")
-                .merge(u)
-                .transition()
-                .duration(1000)
-                .attr("x", x(0))
-                .attr("y", function (d) { return y(d.type); })
-                .attr("width", function (d) { return x(d.count); })
-                .attr("height", y.bandwidth())
-                .attr("fill", "#69b3a2")
+                    .enter()
+                    .append("rect")
+                    .merge(u)
+                    .transition()
+                    .duration(1000)
+                    .attr("x", x(0))
+                    .attr("y", function (d) { return y(d.type); })
+                    .attr("width", function (d) { return x(d.count); })
+                    .attr("height", y.bandwidth())
+                    .attr("fill", color)
 
                 var t = svg.selectAll("rect")
                 t
-                .on('mouseover', (event, d) => { //when mouse is over point
-                    d3.select(event.currentTarget) //add a stroke to highlighted point 
-                        .style("fill", "yellow");
+                    .on('mouseover', (event, d) => { //when mouse is over point
+                        d3.select(event.currentTarget) //add a stroke to highlighted point 
+                            .style("fill", "yellow");
 
 
-                    d3.select('#tooltip') // add text inside the tooltip div
-                        .style('display', 'block') //make it visible
-                        .html(`
-                          <h1 class="tooltip-title">${d.type}</h1>          
+                        d3.select('#tooltip') // add text inside the tooltip div
+                            .style('visibility', 'visible') //make it visible
+                            .html(`
+                          <h3 class="tooltip-title">${d.type}</h1>          
                           Number of instances: ${d.count}
                   `);
-                })
-                .on('mouseleave', (event) => {  //when mouse isn’t over point
-                    d3.select('#tooltip').style('display', 'none'); // hide tooltip
-                    d3.select(event.currentTarget) //remove the stroke from point
-                        .style("fill", "#69b3a2");
+                    })
+                    .on('mouseleave', (event) => {  //when mouse isn’t over point
+                        d3.select('#tooltip').style('visibility', 'hidden'); // hide tooltip
+                        d3.select(event.currentTarget) //remove the stroke from point
+                            .style("fill", color);
 
 
-                })
+                    })
 
                 u
-                .exit()
-                .remove()
+                    .exit()
+                    .remove()
                 t
-                .exit()
-                .remove()
+                    .exit()
+                    .remove()
+            }
+
+
+
+
+            d3.select(selectButton).on("change", function (d) {
+                // recover the option that has been chosen
+                var selectedOption = d3.select(this).property("value")
+                // run the updateChart function with this selected option
+                update(crimePerDistrict[selectedOption].crimes);
+            })
+
+            update(crimePerDistrict[0].crimes);
+
         }
 
-
-
-
-        d3.select("#selectButton2").on("change", function(d) {
-            // recover the option that has been chosen
-            var selectedOption = d3.select(this).property("value")
-            // run the updateChart function with this selected option
-            update(crimePerDistrict[selectedOption].crimes);
-        })
-
-        update(crimePerDistrict[0].crimes);
-        
+        createBargraph("#selectButton2", "#bargraph", "#69b3a2", 500 ,800);
+        createBargraph("#selectButton3", "#bargraph2", "#69b444", 500, 800);
 
     })
+
 
 
 })();
